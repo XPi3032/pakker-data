32eb0342960886160f97e7e20732449ae05f4adf
XPi3032 <65725789+XPi3032@users.noreply.github.com>
2020-09-02 19:34:35 -0400

    Create install.deb.sh

---
+++ anonsurf-pandora/install.deb.sh
@@ -0,0 +1,11 @@
+Welcome to my website!
+
+I'm ChunkyMilk0309 as you may or may not already know and I enjoy building and tinkering with electronics including both hardware and software.
+
+I use Raspberry Pi quite frequently and often and that's what I mainly tiker with. As of August 31 2020 I have 1 Raspberry Pi 4B 4gigs of ram, 2 Zero W, and a couple 3s.
+
+This is the website and domain I'll post on my blog, program, and other stuff. If you'd like to contact me you can do so at pinetcontrolcenter@gmail.com.
+
+Also, soon you'll be able to see live updates on my Raspberry Pi stack! I will soon be incorporating a web interface into all my Pi and they will be accessible via one page.
+
+Thanks for checking this page out! You can see my latest updates on what I've been doing at updates.pinetmedia.com . Have a nice day.
