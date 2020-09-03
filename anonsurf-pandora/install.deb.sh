f39c219fa6c83e319d88a287be52a84f83beb6d8
XPi3032 <65725789+XPi3032@users.noreply.github.com>
2020-09-03 09:44:07 -0400

    Update install.deb.sh

--- anonsurf-pandora/install.deb.sh
+++ anonsurf-pandora/install.deb.sh
@@ -1,11 +1,7 @@
-Welcome to my website!
+git.pinetmedia.com // my Git server. Powered by Gogs and a Raspberry Pi 3B
 
-I'm ChunkyMilk0309 as you may or may not already know and I enjoy building and tinkering with electronics including both hardware and software.
+go.pinetmedia.com  // My link shortener. This is where links are connected to (no IP collection).
 
-I use Raspberry Pi quite frequently and often and that's what I mainly tiker with. As of August 31 2020 I have 1 Raspberry Pi 4B 4gigs of ram, 2 Zero W, and a couple 3s.
+wiki.pinetmedia.com // My wiki
 
-This is the website and domain I'll post on my blog, program, and other stuff. If you'd like to contact me you can do so at pinetcontrolcenter@gmail.com.
-
-Also, soon you'll be able to see live updates on my Raspberry Pi stack! I will soon be incorporating a web interface into all my Pi and they will be accessible via one page.
-
-Thanks for checking this page out! You can see my latest updates on what I've been doing at updates.pinetmedia.com . Have a nice day.
+files.pinetmedia.com // My file storage
